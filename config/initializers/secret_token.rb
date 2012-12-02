1777b1077d11f05f37b347a39828f412a1e06da7
andreaphillips <andrea@agilityfeat.com>
2012-12-02 11:27:54 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mychoresweb::Application.config.secret_token = 'bddff45beec23225d61f344a725ee868795989ede1c4720eaefc66c9e12117712b30956b485936dcc576318fbe70104caafa9184c50878c894e8e04e85f6f0b1'
