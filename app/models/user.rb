962a84d80ae3a61991a4d60daeb0b6ce44775f40
andreaphillips <andrea@agilityfeat.com>
2013-03-25 19:21:56 -0600

    welcome push

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,38 @@ class User < ActiveRecord::Base
 
   validates :email, :presence => true
   #after_create :send_welcome_email
+  after_create :send_welcome_push
 
   def send_welcome_email
     Mailer.signup_notification(self).deliver
   end
+
+  def send_welcome_push
+    pusher = Grocer.pusher(
+        certificate: File.join(Rails.root,"db/certificates/mychorescert.pem"),
+        gateway:     "gateway.sandbox.push.apple.com", # optional; See note below.
+        port:        2195,
+        retries:     5                         # optional
+    )
+
+    page = Page.find_by_name('Welcome!')
+
+    PageUser.create(:user_id => id, :device_token => cloud_id,:page_id => page.id) unless page.nil?
+
+    notification = Grocer::Notification.new(
+        device_token: device,
+        alert: { "body" =>  "Welcome!"}
+    )
+
+    notification.custom = {:acme2 => ["new message", "1"]}
+
+    notification.sound = 'siren.aiff' unless sound.blank?
+    notification.badge = badge.to_i unless badge.blank?
+
+
+    pusher.push(notification)
+
+
+  end
+
 end
