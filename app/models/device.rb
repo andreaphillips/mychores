cac17470c071448395fbd632f7fb89deb35cb6be
andreaphillips <andrea@agilityfeat.com>
2013-04-01 19:06:07 -0600

    delete chore fix

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,7 +2,7 @@ class Device < ActiveRecord::Base
   attr_accessible :identifier
 
   belongs_to :user
-  after_create :send_welcome_push
+  #after_create :send_welcome_push
 
   def send_welcome_push
     pusher = Grocer.pusher(
@@ -11,23 +11,14 @@ class Device < ActiveRecord::Base
         port:        2195,
         retries:     5                         # optional
     )
-
     page = Page.find_by_name('Welcome!')
-
     PageUser.create(:user_id => self.user_id, :device_token => identifier,:page_id => page.id) unless page.nil?
-
     notification = Grocer::Notification.new(
         device_token: identifier,
         alert: { "body" =>  "Welcome!"}
     )
-
     notification.custom = {:acme2 => ["new message", "1"]}
-
     notification.sound = 'default'
-
-
     pusher.push(notification)
-
-
   end
 end
