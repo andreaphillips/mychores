4acf6dab2cf802487e5fc930fa2076f8ef36fca6
andreaphillips <andrea@agilityfeat.com>
2013-04-09 15:36:42 -0600

    other fix for welcome push

--- app/models/device.rb
+++ app/models/device.rb
@@ -19,25 +19,9 @@ class Device < ActiveRecord::Base
   end
 
   def send_welcome_push
-    pusher = Grocer.pusher(
-        certificate: File.join(Rails.root,"db/certificates/mychorescert.pem"),
-        gateway:     "gateway.sandbox.push.apple.com", # optional; See note below.
-        port:        2195,
-        retries:     5                         # optional
-    )
     page = Page.find_by_name('Welcome!')
     puts "after create send welcome push"
     puts identifier
-
     PageUser.create(:user_id => user_id, :device_token => identifier,:page_id => page.id) unless page.nil?
-    if !identifier.include?('nopush')
-      notification = Grocer::Notification.new(
-          device_token: identifier,
-          alert: { "body" =>  "Welcome to MyChores!"}
-      )
-      notification.custom = {:acme2 => ["new message", "1"]}
-      notification.sound = 'default'
-      pusher.push(notification)
-    end
   end
 end
